c9f4ad8ffa782e827522bc15e6a0bb14aba48864
Maciej Zieniuk <zieniuk.maciej@gmail.com>
2020-11-20 15:22:01 +0000

    fix

--- src/main/java/com/lgim/hackathon/team4/location/PostCodeService.java
+++ src/main/java/com/lgim/hackathon/team4/location/PostCodeService.java
@@ -1,10 +1,10 @@
 package com.lgim.hackathon.team4.location;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 
@@ -14,5 +14,5 @@ public interface PostCodeService {
     @GET
     @Path("/{postcode}")
     @Produces("application/json")
-    PostCode getByPostCode(@PathParam String postcode);
+    PostCode getByPostCode(@PathParam("postcode") String postcode);
 }
